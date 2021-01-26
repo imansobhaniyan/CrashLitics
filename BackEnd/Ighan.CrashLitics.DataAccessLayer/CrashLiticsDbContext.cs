fa400703e585f36c90a7c1696e450ffbecc6cc31
Iman Sobhaniyan <sobhaniyan.iman@gmail.com>
2021-01-26 17:46:31 +0330

    init very basic dbContext

---
+++ BackEnd/Ighan.CrashLitics.DataAccessLayer/CrashLiticsDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Ighan.CrashLitics.StorageModels;
+
+using Microsoft.EntityFrameworkCore;
+
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Ighan.CrashLitics.DataAccessLayer
+{
+    public class CrashLiticsDbContext : DbContext
+    {
+        public CrashLiticsDbContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Brand> Brands { get; set; }
+
+        public DbSet<Device> Devices { get; set; }
+
+        public DbSet<Manufacturer> Manufacturers { get; set; }
+
+        public DbSet<Model> Models { get; set; }
+    }
+}
